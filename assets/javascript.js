aea4396dacb7bc429611414ff245a6b5f9c0a58f
Julian Almendarez <ppalmendarez@yahoo.com>
2021-04-08 13:20:33 -0500

    test commit

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,61 +1,20 @@
-// Query selectors for answer choices
-let answerChoice1 = document.getElementById("answer1");
+let startBtnEl = document.getElementById("begin-Btn");
+let answerChoicesEl = document.getElementById("containerQ");
+let correctAnswerEL = document.getElementById("");
+let skipNextEl = document.getElementById("next-Btn");
 
-// score keeper we can start at 0
-// let score = 0
-// save score display until the end
+startBtnEl.addEventListener("click", startGame);
 
-
-//======= timer
-// Make a timer
-// make a button that  starts the timer
-// display the timer (we need to find a spot for it in our html)
-// how do I know how long the timer starts with
-
-// I need a variable for my timer
-// let timer = 40
-// set interval setInterval 1000 
-// remember the activity for colorsplosion
-
-
-
-// ====== ending 
-// something that happens at the end
-// display that the game is over 
-// with a highscore counter
-
-
-
-
-
-// when the quiz is done reset the timer 
-
-
-
-// that button also starts the quiz by showing the first question and answer
-
-
-
-
-// Make an eventListener
-
-// console.log(answerChoice1)
-
-answerChoice1.addEventListener("click", function(event){
-
-if(event.target.textContent === "Red"){
-    console.log("correct")
-    //notify the user if their answer choice was correct on the page
-    // after they answer hide choices and question
-    // display new question and choices
-    // score variable needs to go up by 1
-
-} else{
-    console.log(event.target.textContent)
-    //notify if they chose wrong
-    // after they answer hide choices and question
-    // display new question and choices
-
+function startGame() {
+  startBtnEl.classList.add("hide");
+  answerChoicesEl.classList.remove("hide");
+  startBtnEl.style.display = "none";
 }
-});
 
+function nextQset() {
+  skipNextEl.classList.add("hide");
+  answerChoicesEl.classList.remove("hide");
+  skipNextEl.style.display = "none";
+}
+
+function chooseAnswer() {}
